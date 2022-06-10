830c050f8d00932d2b5fb1e3404790732aa36954
anandrgitnirman <anand.rajamani@nirman.io>
2022-06-10 09:19:36 +0530

    example service enhanced to support training and dynamic calls

--- buildproto.sh
+++ buildproto.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/example_service.proto
+echo `pwd`
+python3  -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. singnet/snet-daemon/pricing/pricing.proto --proto_path=/home/adminaccount/singnet/src
+python3  -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. singnet/snet-daemon/training/training.proto --proto_path=/home/adminaccount/singnet/src
+python3  -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/example_service.proto --proto_path=/home/adminaccount/singnet/src
---
+++ service/model_server.py
@@ -0,0 +1,75 @@
+import sys
+import logging
+import time
+import uuid
+
+import grpc
+import concurrent.futures as futures
+
+import service.common
+
+# Importing the generated codes from buildproto.sh
+import singnet.snet_daemon.training.training_pb2_grpc as grpc_bt_grpc
+from singnet.snet_daemon.training.training_pb2 import  ModelDetailsResponse
+from singnet.snet_daemon.training.training_pb2 import  IN_PROGRESS
+from singnet.snet_daemon.training.training_pb2 import  CREATED
+from singnet.snet_daemon.training.training_pb2 import  COMPLETED
+
+logging.basicConfig(level=10, format="%(asctime)s - [%(levelname)8s] - %(name)s - %(message)s")
+log = logging.getLogger("model_training")
+
+
+# Create a class to be added to the gRPC server
+# derived from the protobuf codes.
+class ModelServicer(grpc_bt_grpc.ModelServicer):
+    def __init__(self):
+
+        # Just for debugging purpose.
+        log.debug("ModelServicer created")
+
+    # The method that will be exposed to the snet-cli call command.
+    # request: incoming data
+    # context: object that provides RPC-specific information (timeout, etc).
+    def create_model(self, request, context):
+        log.debug("Create Model ")
+        # To respond we need to create a Result() object (from .proto file)
+        self.result = ModelDetailsResponse()
+        self.result.model_id = "#CLientgeneratedModelId"
+        self.result.status = CREATED
+        return self.result
+
+    def update_model_access(self, request, context):
+        log.debug("MAY be I Dont need to implement this,update_model_access")
+        # To respond we need to create a Result() object (from .proto file)
+        self.result = ModelDetailsResponse()
+        self.result.status = IN_PROGRESS
+        return self.result
+
+    def delete_model(self, request, context):
+
+        log.debug("MODEL DELETED AT CLIENT END to....")
+        # To respond we need to create a Result() object (from .proto file)
+        self.result = ModelDetailsResponse()
+        self.result.status = COMPLETED
+        return self.result
+
+
+    def get_model_details(self, request, context):
+        # In our case, request is a Numbers() object (from .proto file)
+        log.debug("WHY ASK ME ASK DAEMON get_model_details ....")
+        # To respond we need to create a Result() object (from .proto file)
+        self.result = ModelDetailsResponse()
+        self.result.status = IN_PROGRESS
+        return self.result
+
+    def get_training_status(self, request, context):
+        # In our case, request is a Numbers() object (from .proto file)
+        log.debug("WHY ASK ME ASK DAEMON get_model_details ....")
+        # To respond we need to create a Result() object (from .proto file)
+        self.result = ModelDetailsResponse()
+        self.result.status = IN_PROGRESS
+        return self.result
+
+
+
+
